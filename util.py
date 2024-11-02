a976d00b901f7dff9a1e19b9fcb56a8e64e496dc
Raunak Agarwal <raunakagarwal@Mac-2.lan>
2024-11-02 12:50:54 -0600

    remove comments

--- util.py
+++ util.py
@@ -7,7 +7,6 @@ import bitio
 import huffman
 import pickle
 
-##
 def read_tree(tree_stream):
     '''Read a description of a Huffman tree from the given compressed
     tree stream, and use the pickle module to construct the tree object.
